b7a538f9ee1c166d6bf1df4f343bf89344d03795
Aleksey Zakharov <aleksey-zh1991@mail.ru>
2022-01-06 14:05:02 +0300

    Формула решения уровнения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  if (a==0) return;
+  let d = Math.pow(b,2) - 4*ac;
+  let x1,x2;
+  if (d<0) {
+    return [];
+  } else if (d==0) {
+    x1 = -b/(2*a);
+    return [x1];
+  } else if (d>0) {
+    x1 = (-b + Math.sqrt(d))/(2*a);
+    x2 = (-b - Math.sqrt(d))/(2*a);
+    return [x1,x2];
+  };
+
+  describe('Домашнее задание к занятию 1. «Основные понятия»', () => {
+    describe('Задача №1', () => {
+      it('должна возвращать 2 корня уравнения', () => {
+        expect(solveEquation(1, 5, 4)).toEqual([-1, -4]);
+      });
+    
+      it('должна возвращать 1 корень уравнения', () => {
+        expect(solveEquation(1, 2, 1)).toEqual([-1]);
+      });
+  
+      it('должна вернуть пустой массив', () => {
+        expect(solveEquation(1, 2, 10)).toEqual([]);
+      });
+    });
+  });
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
