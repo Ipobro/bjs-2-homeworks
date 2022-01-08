e3c6124839fcda936dee111fc74cd20130bcce2b
Aleksey Zakharov <aleksey-zh1991@mail.ru>
2022-01-08 14:26:22 +0300

    Убрал тест из функции

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,7 +2,7 @@
 
 function solveEquation(a, b, c) {
   if (a==0) return;
-  let d = Math.pow(b,2) - 4*ac;
+  let d = Math.pow(b,2) - 4*a*c;
   let x1,x2;
   if (d<0) {
     return [];
@@ -13,23 +13,7 @@ function solveEquation(a, b, c) {
     x1 = (-b + Math.sqrt(d))/(2*a);
     x2 = (-b - Math.sqrt(d))/(2*a);
     return [x1,x2];
-  };
-
-  describe('Домашнее задание к занятию 1. «Основные понятия»', () => {
-    describe('Задача №1', () => {
-      it('должна возвращать 2 корня уравнения', () => {
-        expect(solveEquation(1, 5, 4)).toEqual([-1, -4]);
-      });
-    
-      it('должна возвращать 1 корень уравнения', () => {
-        expect(solveEquation(1, 2, 1)).toEqual([-1]);
-      });
-  
-      it('должна вернуть пустой массив', () => {
-        expect(solveEquation(1, 2, 10)).toEqual([]);
-      });
-    });
-  });
+  }
 };
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
