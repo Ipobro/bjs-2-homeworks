a790c335a089c1b2da58612d723d655128d87884
Aleksey Zakharov <aleksey-zh1991@mail.ru>
2022-01-22 15:17:21 +0300

    Функция work и  makeWork

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,8 @@
 // Задание 1
 function getArrayParams(arr) {
 
-  let min = (Math.min(...arr));
-  let max = (Math.max(...arr));
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
   let sum = 0;
 
   for (i = 0; i < arr.length; i++) { 
@@ -19,9 +19,10 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  
+  let sum = arr[0].reduce(function(a, b) {
+    return a + b;
+  });
 
   return sum;
 }
@@ -29,11 +30,17 @@ function worker(arr) {
 function makeWork(arrOfArr, func) {
   let max;
 
-  // Ваш кода
-  // for ...
+  for (i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[0]) > func(arrOfArr[1])) {
+      let max = func(arrOfArr[0]);
+    } else if (func(arrOfArr[1]) > func(arrOfArr[0])) {
+      let max = func(arrOfArr[1]);
+    };
+  };
+
   
   return max;
-}
+};
 
 // Задание 3
 function worker2(arr) {
